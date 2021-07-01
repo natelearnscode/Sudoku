8b0188173934ccd49817ba08ebfe3a86da4efbf0
natemeshesha <natemesheshatech@gmail.com>
2021-06-30 23:25:51 -0500

    created game board

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,54 @@
-const board_border = 'black';
-const board_background = "rgb(200,200,200)";
+class Game {
+    /* Public */
+    static startGame() {
+        this.#initializeGame();
+    }
 
-const board = document.getElementById("gameCanvas");
-const board_ctx = board.getContext("2d");
+    /* Private */
+    static #table;
+    static #gameBoard;
+    static #sudokuSolver;
+    static #activeCell;
 
-board.style.border = "10px solid black"
-board_ctx.fillStyle = board_background;
-board_ctx.fillRect(0, 0, board.width, board.height);
+    static #initializeGame() {
+        //generate game board
+        this.#gameBoard = this.#generateSudokuBoard();
+
+        //create Table
+        this.#table = document.getElementById("sudoku-table");
+        for(let i = 0; i < 9; i++) {
+            let row = this.#table.insertRow();
+            for(let j = 0; j < 9; j++) {
+                let cell = row.insertCell();
+                cell.innerHTML = this.#gameBoard[i][j];
+                cell.onclick = this.#changeActiveCell.bind(this);
+            }
+        }
+
+        //initialize event listener
+        document.addEventListener('keydown', this.#handleInput.bind(this));
+    }
+
+    static #generateSudokuBoard() {
+        console.log("generating sudoku board...");
+        return new Array(9).fill("&nbsp").map(()=>new Array(9).fill("&nbsp;"));
+    };
+
+    static #handleInput(event) {
+        //Only handle 1-9 keys and if a cell has been selected
+        if (isFinite(event.key) && event.key != 0 && this.#activeCell != null) { 
+            //1-9 keys only
+            this.#activeCell.innerHTML = event.key.toString();
+        }
+    };
+
+    static #changeActiveCell(e) {
+        if (this.#activeCell != null) {
+            //Reset background color to white before changing active cell
+            this.#activeCell.style.background = "white";
+        }
+        this.#activeCell = e.path[0];
+        //Give grey background to active cell
+        this.#activeCell.style.background = "rgb(240,240,240)";
+    }
+}
